0c746eeda051a5a93fc7bd5ef40f41757036d722
StarterProjectMasterRepo <reddymasu@gmail.com>
2024-01-09 16:33:41 -0600

    Tuesday-CW

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,16 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { StatisticsComponent } from './statistics/statistics.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CircularMenuComponent } from './circular-menu/circular-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, StatisticsComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, StatisticsComponent, MatSidenavModule, MatIconModule, MatListModule, MatToolbarModule, CircularMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/circular-menu/circular-menu.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-circular-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './circular-menu.component.html',
+  styleUrl: './circular-menu.component.css'
+})
+export class CircularMenuComponent {
+
+  isMenuOpen = false;
+
+  toggleMenu(): void{
+    this.isMenuOpen = !this.isMenuOpen;
+    }
+}
